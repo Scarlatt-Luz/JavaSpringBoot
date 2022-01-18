5b7593e17028b50a9e9bbad6e3b4baf5cbc9fd91
Scarlatt Luz <contatoscarlatt@gmail.com>
2022-01-17 21:36:30 -0300

    Exercícios finalizados

--- helloworld/src/main/java/com/generation/helloworld/controller/HelloController.java
+++ helloworld/src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -17,4 +17,14 @@ public class HelloController {
 	public String hello2() {
 		return "Hi Família 41!!";
 	}
+	
+	@GetMapping("/habilidade/mentalidade")
+	public String habMent() {
+		return "Utilizei a mentalidade de Persistência e a habilidade de atenção aos detalhes para realizar essa atividade.";
+	}
+	
+	@GetMapping("/objetivos/aprendizagem")
+	public String objAprend() {
+		return "O meu objetivo de aprendizagem para essa semana é manter a minha atenção aos detalhes e melhorar a minha mentalidade de crescimento.";
+	}
 }
