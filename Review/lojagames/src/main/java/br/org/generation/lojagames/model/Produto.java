92682450fdfc38c6c0377dde044ed50232aa789f
Scarlatt Luz <contatoscarlatt@gmail.com>
2022-01-24 16:45:07 -0300

    Exercício de revisão 1

---
+++ Review/lojagames/src/main/java/br/org/generation/lojagames/model/Produto.java
@@ -0,0 +1,129 @@
+package br.org.generation.lojagames.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O atributo console é Obrigatório!")
+	@Size(max=20, message="O atributo título deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String console;
+	
+	@NotBlank(message = "O atributo nome é Obrigatório!")
+	@Size( min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String nome;
+
+	@NotBlank(message = "O atributo descrição é Obrigatório!")
+	@Size( min = 10, max = 1000, message = "O atributo descrição deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String descricao;
+	
+	@NotBlank
+	private int quantidade;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING)
+	@NotBlank
+	private BigDecimal preco;
+	
+	@Size(max = 100, message = "O atributo foto deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String foto;
+	
+	@UpdateTimestamp
+	private LocalDate dataLancamento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+	
+}
